74dd00f950ddbd8012e1fbba216585f326630d56
bdkopen <bdkopen@gmail.com>
2017-04-09 12:43:44 -0400

    Global header working
    Setup the global header to work fully no matter the subpage.

--- javascript/header-bar.js
+++ javascript/header-bar.js
@@ -1,3 +1,5 @@
 var headerbar = document.getElementById("header-bar");
+var href = document.location.href;
+var current_dir = href.substr(href.lastIndexOf('/'), 0);
 
-headerbar.innerHTML = '<div class="dropdown"><button class="dropbtn">Games</button><div class="dropdown-content"><a href="universes/index.html">Universe Generator</a><a href="#">Coming soon...</a></div></div><div class="dropdown"><button class="dropbtn">Tools</button><div class="dropdown-content"><a href="nameGenerator/index.html">Name Generator</a><a href="#">Coming soon...</a></div></div>';
+headerbar.innerHTML = '<div class="dropdown"><a href=' + current_dir + '><button class="dropbtn">Home</button></a></div><div class="dropdown"><button class="dropbtn">Games</button><div class="dropdown-content"><a href="' + current_dir + '/universes/index.html">Universe Generator</a><a href="#">Coming soon...</a></div></div><div class="dropdown"><button class="dropbtn">Tools</button><div class="dropdown-content"><a href="' + current_dir + '/nameGenerator/index.html">Name Generator</a><a href="#">Coming soon...</a></div></div>';
