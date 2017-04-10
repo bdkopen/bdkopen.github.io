347a4ceb10ee303e5409665e02a53901da80ea14
bdkopen <bdkopen@gmail.com>
2017-04-10 18:52:45 -0400

    Header bar fix
    Made the header bar clicks/interactions work correctly no matter what page/subpage/subpage of a subpage it is.

--- javascript/header-bar.js
+++ javascript/header-bar.js
@@ -1,5 +1,14 @@
 var headerbar = document.getElementById("header-bar");
-var href = document.location.href;
-var current_dir = href.substr(href.lastIndexOf('/'), 0);
 
-headerbar.innerHTML = '<div class="dropdown"><a href=' + current_dir + '><button class="dropbtn">Home</button></a></div><div class="dropdown"><button class="dropbtn">Games</button><div class="dropdown-content"><a href="' + current_dir + '/universes/index.html">Universe Generator</a><a href="#">Coming soon...</a></div></div><div class="dropdown"><button class="dropbtn">Tools</button><div class="dropdown-content"><a href="' + current_dir + '/nameGenerator/index.html">Name Generator</a><a href="#">Coming soon...</a></div></div>';
+//Old attempt at running the url clicks.
+/*
+var href = document.location.href;
+//lastIndexOf determines the value inwhich the last / is placed.
+//substr finds the range between two values.
+var current_dir = href.substr(0, href.lastIndexOf('/index.html'));
+console.log(current_dir);*/
+
+//headerbar.innerHTML = '<div class="dropdown"><a href=' + current_dir + '><button class="dropbtn">Home</button></a></div><div class="dropdown"><button class="dropbtn">Games</button><div class="dropdown-content"><a href="' + current_dir + '/universes/index.html">Universe Generator</a><a href="#">Coming soon...</a></div></div><div class="dropdown"><button class="dropbtn">Tools</button><div class="dropdown-content"><a href="' + current_dir + '/nameGenerator/index.html">Name Generator</a><a href="#">Coming soon...</a></div></div>';
+
+//Workinng url clicks header bar.
+headerbar.innerHTML = '<div class="dropdown"><a href="/"><button class="dropbtn">Home</button></a></div><div class="dropdown"><button class="dropbtn">Games</button><div class="dropdown-content"><a href="/universes/index.html">Universe Generator</a><a href="#">Coming soon...</a></div></div><div class="dropdown"><button class="dropbtn">Tools</button><div class="dropdown-content"><a href="/nameGenerator/index.html">Name Generator</a><a href="#">Coming soon...</a></div></div>';
