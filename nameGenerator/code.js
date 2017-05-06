4ca72cc349b87d8e655669295539995edcf428f6
bdkopen <bdkopen@gmail.com>
2017-05-06 17:22:17 -0400

    Complete random letter generation.

--- nameGenerator/code.js
+++ nameGenerator/code.js
@@ -1,8 +1,22 @@
 var outputBox = document.getElementById("output");
 var generateTownName = document.getElementById("generateTownName");
-var townNameStart = ["Ji", "Ri", "Ripp"];
-var townNameMiddle = ["vin", "il", "el"];
-var townNameEnd = ["ia", "oria", "land", "orp", ""];
+
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+var randomLetter = function(letterCase) {
+	if(letterCase === "C") {
+		return alphabet[1/*(Math.random() * (alphabet.length-1))*/];
+	} else if(letterCase === "L") {
+		return alphabet[1/*(Math.random() * (alphabet.length-1))*/];
+	} else {
+		console.log("randomLetter function failed to generate a random letter. No set case")
+	}
+};
+
+var townNameStart = ["Ji", "Ri", "Li", "We", "Wete", "Lakeside Zi", "Za", "Mount "+randomLetter("C")+randomLetter("L")];
+var townNameMiddle = ["vin", "zin", "zil", "arian"];
+var townNameEnd = ["ia", "oria", "land", "orp", "aria"];
 
 function getTownName(num) {
 	outputBox.innerHTML = "";
