769b0b9131194135363b1fc3e7117a8a48f53d64
bdkopen <bdkopen@gmail.com>
2017-08-08 20:15:42 -0400

    Created a new random page. Added it too header bar.

---
+++ random/infinite_lawn_mower/code.js
@@ -0,0 +1,26 @@
+var line = [document.getElementById("line1"),document.getElementById("line2"),document.getElementById("line3"),document.getElementById("line4"),document.getElementById("line5"),document.getElementById("line6"),document.getElementById("line7"),document.getElementById("line8"),document.getElementById("line9")];
+
+var ceneter = document.getElementById("center");
+
+var keepInView = function() {
+	
+	center.scrollRight = center.scrollWidth;
+}
+
+var expandLawn = function() {
+	for(var a = 0; a < line.length; a++) {
+		if(a > -1 && a < 7) {
+			line[a].innerHTML = "&nbsp;" + line[a].innerHTML + "&nbsp;";
+		} else if(a === 7){
+			line[a].innerHTML = "&nbsp;" + line[a].innerHTML + "/";
+		} else if(a === 8){
+			line[a].innerHTML = "^" + line[a].innerHTML + "^";
+		}
+	}
+	
+	keepInView();
+	setTimeout(expandLawn, 100);
+}
+
+
+expandLawn();
